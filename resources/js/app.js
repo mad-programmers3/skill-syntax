7af8fd1c38b6989eb71615434eaf3f841bc7c857
mdshihabuddin78 <shihab110897@gmail.com>
2024-09-19 09:46:32 +0600

    laravel_vue_mastering

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-import './bootstrap';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter);
+import App from './App.vue'
+import route from './routes'
+
+const router = new VueRouter({
+    mode : 'history',
+    routes : route,
+    linkActiveClass : 'active'
+});
+
+
+const vue = new Vue({
+    el : '#app',
+    components : {App},
+    router
+});
---
+++ resources/js/routes/index.js
@@ -0,0 +1,10 @@
+import DashboardComponent from "../views/DashboardComponent";
+
+const routes =[
+    {
+        path: '/admin/dashboard',
+        name:'home',
+        component: DashboardComponent
+    }
+];
+export default routes;
